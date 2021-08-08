be025b487a355df172e5ff6ed8c04f24edd89c8c
BoyKaYK <88595925+BoyKaYK@users.noreply.github.com>
2021-08-08 13:41:36 +0200

    Add files via upload

---
+++ hypercube.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int cube() {
+	int d, n;
+	cin >> n >> d;
+	
+	int long long newton = 1;     
+
+	for (unsigned int i = 1; i <= d; i++)   
+	{
+		newton = newton * (n - i + 1) / i;  
+	}
+	
+
+	unsigned long long odp = newton * pow(2, n - 1);
+
+	cout << odp <<"\n" ;
+
+	return 0;
+}
+int main() {
+	int n; // podejscia  
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cube();
+		
+	}
+
+	
+	return 0;
+}
